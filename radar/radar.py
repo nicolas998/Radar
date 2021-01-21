7503474ca757bf43cd7122baf0106afa88c9760d
nicolas998 <nicolas.velasquezgiron@gmail.com>
2021-01-20 20:58:31 -0600

    errores de indentacion

--- radar/radar.py
+++ radar/radar.py
@@ -516,9 +516,8 @@ class radar_process:
 		peaks,self.ConvStra = radar_f90.steiner_find_peaks(self.ref,
 			umbral,radio,metNum,ZminSiriluk,a_yuter,b_yuter,
 			int(self.ref.shape[0]), int(self.ref.shape[1]))
-	
-	def save_rain_class(self, ruta, ExtraVar = None, ArrayVar1 = None, 
-                    ArrayVar2=None):
+
+	def save_rain_class(self, ruta, ExtraVar = None, ArrayVar1 = None,ArrayVar2=None):
 		gr = Dataset(ruta,'w',format='NETCDF4')
 		'Descripcion: Guarda datos de radar procesados\n'\
 		'\n'\
@@ -534,22 +533,23 @@ class radar_process:
 		'Retornos\n'\
 		'----------\n'\
 		'self : Con las variables iniciadas.\n'\
-                #Diccionario de propiedades
-		Dict = {'ncols':RadProp[0],
-                    'nrows': RadProp[1],
-                    'xll': RadProp[2],
-                    'yll': RadProp[3],
-                    'dx': RadProp[4]}
-                #Establece tamano de las variables 
-		DimNcol = gr.createDimension('ncols',self.ConvStra.shape[0])
-		DimNfil = gr.createDimension('nrows',self.ConvStra.shape[1])
-                #variables del arrayVar
-                if type(ArrayVar1) is dict:
-                    k = ArrayVar1.keys()[0]
-                    DimArray = gr.createDimension('narray1',ArrayVar1[k]['Data'].size)
-                if type(ArrayVar2) is dict:
-                    k = ArrayVar2.keys()[0]
-                    DimArray = gr.createDimension('narray2',ArrayVar2[k]['Data'].size)
+
+        #Diccionario de propiedades
+        Dict = {'ncols': RadProp[0],   
+                'nrows': RadProp[1],
+                'xll': RadProp[2],
+                'yll': RadProp[3],
+                'dx': RadProp[4]}
+        #Establece tamano de las variables 
+        DimNcol = gr.createDimension('ncols',self.ConvStra.shape[0])
+        DimNfil = gr.createDimension('nrows',self.ConvStra.shape[1])
+        #variables del arrayVar
+        if type(ArrayVar1) is dict:
+            k = ArrayVar1.keys()[0]
+            DimArray = gr.createDimension('narray1',ArrayVar1[k]['Data'].size)
+        if type(ArrayVar2) is dict:
+            k = ArrayVar2.keys()[0]
+            DimArray = gr.createDimension('narray2',ArrayVar2[k]['Data'].size)
 		#Crea variables
 		ClasStruct = gr.createVariable('Conv_Strat','i4',('ncols','nrows'),zlib=True)
 		ClasRain = gr.createVariable('Rain', 'i4', ('ncols','nrows'),zlib=True)
@@ -570,23 +570,23 @@ class radar_process:
 		ppt = ppt.astype(float)
                 ClasRainLow[:] = ppt
 		#Extra veriables 
-                if type(ExtraVar) is dict:
-                    for k in ExtraVar.keys():
-                        Var = gr.createVariable(k,ExtraVar[k]['type'],('ncols','nrows'),zlib=True)
-			Var[:] = ExtraVar[k]['Data']
-                #ArrayVar
-                if type(ArrayVar1) is dict:
-                    for k in ArrayVar1.keys():
-                        var = gr.createVariable(k,ArrayVar1[k]['type'],('narray1',),zlib=True)
-                        var[:] = ArrayVar1[k]['Data'] 
-                if type(ArrayVar2) is dict:
-                    for k in ArrayVar2.keys():
-                        var = gr.createVariable(k,ArrayVar2[k]['type'],('narray2',),zlib=True)
-                        var[:] = ArrayVar2[k]['Data']
-                #Cierra el archivo 
+        if type(ExtraVar) is dict:
+            for k in ExtraVar.keys():
+                Var = gr.createVariable(k,ExtraVar[k]['type'],('ncols','nrows'),zlib=True)
+            Var[:] = ExtraVar[k]['Data']
+        #ArrayVar
+        if type(ArrayVar1) is dict:
+            for k in ArrayVar1.keys():
+                var = gr.createVariable(k,ArrayVar1[k]['type'],('narray1',),zlib=True)
+                var[:] = ArrayVar1[k]['Data'] 
+        if type(ArrayVar2) is dict:
+            for k in ArrayVar2.keys():
+                var = gr.createVariable(k,ArrayVar2[k]['type'],('narray2',),zlib=True)
+                var[:] = ArrayVar2[k]['Data']
+            #Cierra el archivo 
 		gr.setncatts(Dict)
 		gr.close()
-				
+
 	# Genera kernels circulares 
 	def CircKernel(self,radio):
 		#Centro del kernel y cantidad de datos
@@ -713,6 +713,9 @@ class draw_func:
 				#s=30,
 				linewidth=1,)
 				#edgecolor='black')
+                w
+                t
+                u
 		if texto!=None:
 			pl.annotate(texto, xy=(0.1, 0.9), xycoords='axes fraction', size=16)
 		if ruta<>None:
